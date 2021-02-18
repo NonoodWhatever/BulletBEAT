7f3a14153a14a1a5f9e25ea97b66f69e1e689060
Nonood Whatever <kamik.suwannaratt@gmail.com>
2021-02-18 19:23:45 +0700

    Update, Arrives the Bare bone main menu and the emerc ESC exit button.

---
+++ Assets/Script/GetLost.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GetLost : MonoBehaviour
+{
+    // Emerc Exit Program button.
+    void Update()
+    {
+            if (Input.GetKey(KeyCode.Escape))
+            {
+                Application.Quit();
+                print("Leaving with ESC");
+            }
+    }
+    // Literally taken from my old project, as it is the most basic code ever
+
+}
+
---
+++ Assets/Script/MainMenu.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class MainMenu : MonoBehaviour
+{
+    // I created this just to make Main menu work.
+   private void Update() { Cursor.visible = true; }
+    public void Level1()
+    { SceneManager.LoadScene(1); }
+    public void Exit()
+    { Application.Quit();  print("LEAVING, OH WAIT"); }
+}

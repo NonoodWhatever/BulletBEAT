31d1f976129c87bd7d010326371ba809a2faf087
Yean-Star <77831421+Yean-Star@users.noreply.github.com>
2021-02-24 16:46:24 +0700

    Add files via upload

---
+++ Assets/Script/Shooting.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooting : MonoBehaviour
+{
+    public bool canshoot = false;
+    public Transform firepoint;
+    public GameObject bulletprefab;
+    // Start is called before the first frame update
+    public float bulletforce = 5f;
+
+    // Update is called once per frame
+    void Start()
+    {
+        StartCoroutine(Tool());
+    }
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            if(canshoot == true)
+            {
+                Shoot();
+            }
+                
+        }
+    }
+
+    void Shoot()
+    {
+        GameObject bullet = Instantiate(bulletprefab, firepoint.position, firepoint.rotation);
+        Rigidbody2D rb =  bullet.GetComponent<Rigidbody2D>();
+        rb.AddForce(firepoint.up * bulletforce, ForceMode2D.Impulse);
+    }
+
+    IEnumerator Tool()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(0.48f);
+            canshoot = true;
+            Debug.Log("Shoot");
+            yield return new WaitForSeconds(0.2f);
+            canshoot = false;
+        }
+    }
+}

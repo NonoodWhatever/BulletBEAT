390961e1de8185a76d7369325ccd9a532706a388
Yean-Star <77831421+Yean-Star@users.noreply.github.com>
2021-03-14 04:04:36 +0700

    Add files via upload

---
+++ Assets/Script/Beatscroll.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Beatscroll : MonoBehaviour
+{
+    bool trigger = false;
+    private  float tempo = 120f;
+    public bool gamestart;
+    // Start is called before the first frame update
+    void Start()
+    {
+        tempo = tempo / 60f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.anyKeyDown)
+        {
+            gamestart = true;
+        }
+        else
+        {
+            transform.position -= new Vector3(tempo * Time.deltaTime, 0.0f, 0.0f);
+            if(transform.position.x < -10.0f)
+            {
+                Destroy(gameObject);
+            }
+        }
+        if (Input.GetButtonDown("Fire1"))
+        {
+            if (trigger == true)
+            {
+
+                Destroy(gameObject);
+
+
+            }
+
+        }
+
+    }
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Activator")
+        {
+            Gamecontroller.canshoot = true;
+            trigger = true;
+        }
+
+    }
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.tag == "Activator")
+        {
+            Gamecontroller.canshoot = false;
+            trigger = false;
+        }
+
+    }
+}
